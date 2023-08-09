73ec0e580482ea4eb60f3ddce59235266fa5d219
kellraym <ray.al.kell@gmail.com>
2023-08-09 14:09:04 -0600

    adding debug stuff

---
+++ ch_1/do_something/target/debug/deps/do_something-3ee0727ded7c6c02.d
@@ -0,0 +1,8 @@
+/Users/jackquinn/vs_code/rust_class/the_book/ch_1/do_something/target/debug/deps/do_something-3ee0727ded7c6c02.rmeta: src/main.rs Cargo.toml
+
+/Users/jackquinn/vs_code/rust_class/the_book/ch_1/do_something/target/debug/deps/do_something-3ee0727ded7c6c02.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
